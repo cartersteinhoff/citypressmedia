5a8ade7e041768cb1b751cb353f5eb5bcba21831
Carter Steinhoff <cartersteinhoff@gmail.com>
2024-10-16 15:06:09 -0700

    bug fix

--- src/app/dashboard/partner/new/page.tsx
+++ src/app/dashboard/partner/new/page.tsx
@@ -1,6 +1,6 @@
 import { CONFIG } from 'src/config-global';
 
-import { PartnerCreateView } from 'src/sections/Partner/view';
+import { PartnerCreateView } from 'src/sections/partner/view';
 
 // ----------------------------------------------------------------------
 
